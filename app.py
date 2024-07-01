00ac527052ec5cd6bb0e8534fcda47bc8fad9a4d
radcool22 <kabirgupta2201@gmail.com>
2024-07-01 11:03:56 +0530

    defined a simple main function to upload the pdf file

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 
 with st.sidebar:
+    # The sidebar with additional information about the website
     st.title("Chat with Report Cards")
     st.markdown("""
     ## About
@@ -9,3 +11,22 @@ with st.sidebar:
     """)
 
     st.write("Made by Kabir Gupta")
+
+def main():
+    # Prints the name of the product
+    st.header("Assessment Insight")
+
+    # Lets the user upload a PDF report
+    pdf = st.file_uploader("Upload your report card here", type="pdf")
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+        text = ""
+        for page in pdf_reader.pages:   
+            text += page.extract_text()
+
+        st.write(text)
+
+if __name__ == "__main__":
+    main()
