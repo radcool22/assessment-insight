d88552ddf521f1d302e6f8a3e5ce197f8904a314
radcool22 <kabirgupta2201@gmail.com>
2024-07-01 10:12:45 +0530

    simple sidebar made using streamlit import

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+with st.sidebar:
+    st.title("Chat with Report Cards")
+    st.markdown("""
+    ## About
+    Hi! I am a student studying in Woodstock School 
+    . This app is powered by Streamlit, OpenAI, and Langchain.
+    """)
+
+    st.write("Made by Kabir Gupta")
