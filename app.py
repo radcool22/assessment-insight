ba02a1667af8453a662eb6d29d685ab9a3acb524
radcool22 <kabirgupta2201@gmail.com>
2024-07-02 11:33:12 +0530

    added embeddings for the chunks created and made a vector store variable using FAISS to read text

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 with st.sidebar:
     # The sidebar with additional information about the website
@@ -19,20 +21,28 @@ def main():
 
     # Lets the user upload a PDF report
     pdf = st.file_uploader("Upload your report card here", type="pdf")
+    st.write(pdf.name)
 
     if pdf is not None:
-        pdf_reader = PdfReader(pdf)
+        pdf_reader = PdfReader (pdf)
 
         text = ""
         for page in pdf_reader.pages:   
             text += page.extract_text()
 
+        
+        # Splits the text into readable chunks
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size = 1000,
             chunk_overlap = 200,
             length_function = len 
         )
+        chunks = text_splitter.split_text(text=text)
 
+        # Creates embeddings for the chunks
+        embeddings = OpenAIEmbeddings()
+
+        VectorStore = FAISS.from_texts(chunks, embeddings)
 
 if __name__ == "__main__":
     main()
