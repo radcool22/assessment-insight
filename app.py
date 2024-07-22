24c28dd8be778ed61370d7c8d3752b2f3af8f026
radcool22 <kabirgupta2201@gmail.com>
2024-07-22 13:04:26 +0530

    finished first protoype of final product

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
-import sys
-import pickle 
 import openai as openai_original
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -33,7 +31,7 @@ def main(prompt):
 
     pdf = st.file_uploader("Upload your report card here", type="pdf")
     query = st.text_input("Ask a question about IB or your report: ")
-    
+
     def web_query():
         llm = openai.OpenAI(temperature=0.5)
         tools = load_tools(["serpapi"], llm=llm)
@@ -62,38 +60,24 @@ def main(prompt):
     def doc_query():
         if pdf is not None:
             pdf_reader = PdfReader(pdf)
-
             text = ""
             for page in pdf_reader.pages:
                 text += page.extract_text()
 
             # Splits the text into readable chunks
             text_splitter = RecursiveCharacterTextSplitter(
+                separators="\n",
                 chunk_size = 1000,
                 chunk_overlap = 200,
                 length_function = len 
             )
-            chunks = text_splitter.split_text(text=text)
-            store_name = pdf.name[:-4]
-
-            if os.path.exists(f"{store_name}.pkl"):
-                st.write("Embeddings loading from the Disk")
-                with open(f"{store_name}.pkl", "rb") as f:
-                    VectorStore = pickle.load(f)
-            else:
-                embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-                try:
-                    VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-                except TimeoutError as e:
-                    st.error("Timeout error occurred while creating VectorStore. Please try again later.")
-                with open(f"{store_name}.pkl", "wb") as f:
-                    st.write("Creating a new pickle file")
-                    print(VectorStore)
-                    pickle.dump(VectorStore, f)
+            chunks = text_splitter.split_text(text)
+            embeddings = OpenAIEmbeddings()
+            vectorstore = FAISS.from_texts(chunks, embeddings)
 
             if query:
-                docs = VectorStore.similarity_search(query=query, k=3)
-                llm = openai.OpenAI(model_name="gpt-3.5-turbo")
+                docs = vectorstore.similarity_search(query=query, k=3)
+                llm = openai.OpenAI()
                 chain = load_qa_chain(llm=llm, chain_type="stuff")
                 with get_openai_callback() as cb:
                     response = chain.run(input_documents=docs, question=query)
@@ -115,17 +99,15 @@ def main(prompt):
 if __name__ == "__main__":
     prompt = """
     Follow these instructions without fail. 
-    1. Do not deviate from the given instructions. 
-    2. Act like an International Baccalaureate counselor. 
-    3. You should know everything about the IB. 
-    4. Refer to official IB websites.
-    5. You have to explain and answers questions about IB concepts to a parent or a student in simple terms without any fancy jargon or words and connect it with the IB vocabulary. 
-    6. Use extremely simple language to convey the definition. 
-    7. Always provide short and concise answers instead of lengthy ones. 
-    8. The output should not exceed 200-300 words.  
-    9. Avoid excessive bullet points and prioritize concise paragraphs. 
-    10. Follow this without exception. 
-    11. Your role is to assess a students performance based on a report card and give them advice on their performance and provide strengths and weaknesses and grade their assessment and provide their summary in a readable manner. 
-    12. You should be able to help the student to understand and improve based on the report uploaded.
+    1. Act like an International Baccalaureate counselor. 
+    2. You should know everything about the IB. 
+    3. Refer to official IB websites.
+    4. You have to explain and answers questions about IB concepts to a parent or a student in simple terms without any fancy jargon or words and connect it with the IB vocabulary. 
+    5. Use extremely simple language to convey the definition. 
+    6. Always provide short and concise answers instead of lengthy ones. 
+    7. The output should not exceed 200-300 words.  
+    8. Avoid excessive bullet points and prioritize concise paragraphs. 
+    9. Your role is to assess a students performance based on a report card and give them advice on their performance and provide strengths and weaknesses and grade their assessment and provide their summary in a readable manner. 
+    10. You should be able to help the student to understand and improve based on the report uploaded.
     """
     main(prompt)
