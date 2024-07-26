1a8cd08406ce99635622e5252f678cd9aa3d4a9d
radcool22 <kabirgupta2201@gmail.com>
2024-07-26 11:28:24 +0530

    undid previous code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import openai as openai_original
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import openai
 from langchain.vectorstores import FAISS
 from langchain.agents import load_tools
